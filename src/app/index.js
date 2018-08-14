f6de03c69dbe5b4abde9ebaf21a6ef3f7455ee5e
John Furtado <jf8846@gmail.com>
2018-08-14 19:10:09 -0400

    react class to inject html

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('it works?!')
+var React = require('react')
+var createReactClass = require('create-react-class')
+var ReactDOM = require('react-dom')
+
+// component
+var TodoComponent = createReactClass({
+  render: function() {
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+})
+
+// insert component into html
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'))
