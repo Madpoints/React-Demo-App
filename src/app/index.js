0832b8704bc10ccf81bf2d27d2ca6749617e5078
John Furtado <jf8846@gmail.com>
2018-08-17 19:12:37 -0400

    change component state

--- src/app/index.js
+++ src/app/index.js
@@ -6,13 +6,20 @@ var ReactDOM = require('react-dom')
 var TodoComponent = createReactClass({
   getInitialState: function() {
     return {
-      todos: ['code more', 'and more', 'and some more']
+      todos: ['code more', 'and more', 'and some more'],
+      month: 'August'
     }
   },
   render: function() {
+    // bind this funxtion to 'this' component
+    var changeMonth = setTimeout(function() {
+      this.setState({
+        month: "September"
+      })
+    }.bind(this), 5000) // changes month after 5 seconds
     return(
       <div id="todo-list">
-        <p>Need to do:</p>
+        <p>Need to do for {this.state.month}:</p>
         <ul>
           <li>{this.state.todos[0]}</li>
           <li>{this.state.todos[1]}</li>
